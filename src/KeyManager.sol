f45fab0fa81b3f879a477b6d96c7d626182328ef
georgiypetrov <petrovgeorgiy11@gmail.com>
2024-10-02 17:57:33 +0400

    refactor: redundant key management

--- src/KeyManager.sol
+++ src/KeyManager.sol
@@ -10,70 +10,27 @@ abstract contract KeyManager is MiddlewareStorage {
     using ArrayWithTimes for ArrayWithTimes.Address;
 
     error DuplicateKey();
-    error NotExistKey();
-    error NoOperatorKey();
 
-    mapping(address => bytes) public keys;
-    mapping(bytes => ArrayWithTimes.Address) internal keyData;
+    mapping(address => bytes32) public keys;
+    mapping(bytes32 => ArrayWithTimes.Address) internal keyData;
 
-    function operatorByKey(bytes memory key) external view returns (address) {
+    function operatorByKey(bytes32 key) public view returns (address) {
         return keyData[key].getAddress();
     }
 
-    function operatorKey(address operator) external view returns (bytes memory) {
+    function operatorKey(address operator) public view returns (bytes32) {
         return keys[operator];
     }
 
-    function keyWasActiveAt(bytes memory key, uint48 timestamp) external view returns (bool) {
+    function keyWasActiveAt(bytes32 key, uint48 timestamp) public view returns (bool) {
         return keyData[key].wasActiveAt(timestamp);
     }
 
-    function registerKey(address operator, bytes memory key) external onlyOwner {
+    function updateKey(address operator, bytes32 key) external onlyOwner {
         if (keyData[key].getAddress() != address(0)) {
             revert DuplicateKey();
         }
 
-        keys[operator] = key;
-        keyData[key].set(operator);
-    }
-
-    function pauseKey(bytes memory key) external onlyOwner {
-        if (keyData[key].getAddress() == address(0)) {
-            revert NotExistKey();
-        }
-
-        keyData[key].disable();
-    }
-
-    function unpauseKey(bytes memory key) external onlyOwner {
-        if (keyData[key].getAddress() == address(0)) {
-            revert NotExistKey();
-        }
-
-        keyData[key].checkUnpause(SLASHING_WINDOW);
-        keyData[key].enable();
-    }
-
-    function unregisterKey(bytes memory key) external onlyOwner {
-        if (keyData[key].getAddress() == address(0)) {
-            revert NotExistKey();
-        }
-
-        keyData[key].checkUnregister(SLASHING_WINDOW);
-        delete keys[keyData[key].getAddress()];
-        delete keyData[key];
-    }
-
-    function updateKey(address operator, bytes memory key) external onlyOwner {
-        if (keyData[keys[operator]].getAddress() == address(0)) {
-            revert NoOperatorKey();
-        }
-
-        if (keyData[key].getAddress() != address(0)) {
-            revert DuplicateKey();
-        }
-
-        keyData[keys[operator]].checkUnregister(SLASHING_WINDOW);
         keys[operator] = key;
         keyData[key].set(operator);
     }
