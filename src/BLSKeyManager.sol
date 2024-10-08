6b5baf0a48729f7acc2d74b3ecad359271792eab
georgiypetrov <petrovgeorgiy11@gmail.com>
2024-10-09 01:56:36 +0400

    fix: optimize our data structure with times, add vault init check, valid hints in slash, keys determinism

--- src/BLSKeyManager.sol
+++ src/BLSKeyManager.sol
@@ -4,21 +4,27 @@ pragma solidity 0.8.25;
 import {Time} from "@openzeppelin/contracts/utils/types/Time.sol";
 
 import {MiddlewareStorage} from "./MiddlewareStorage.sol";
-import {ArrayWithTimes} from "./libraries/ArrayWithTimes.sol";
+import {EnumerableSetWithTimeData} from "./libraries/EnumerableSetWithTimeData.sol";
 
 abstract contract BLSKeyManager is MiddlewareStorage {
-    using ArrayWithTimes for ArrayWithTimes.Address;
+    using EnumerableSetWithTimeData for EnumerableSetWithTimeData.Inner;
 
     error DuplicateBLSKey();
+    error FreshBLSKey();
 
     mapping(address => bytes) public blsKeys;
-    mapping(bytes => ArrayWithTimes.Address) internal blsKeyData;
+    mapping(address => bytes) public prevBLSKeys;
+    mapping(bytes => EnumerableSetWithTimeData.Inner) internal blsKeyData;
 
     function operatorByBLSKey(bytes memory key) public view returns (address) {
         return blsKeyData[key].getAddress();
     }
 
     function operatorBLSKey(address operator) public view returns (bytes memory) {
+        if (blsKeyData[blsKeys[operator]].enabled == Time.timestamp()) {
+            return prevBLSKeys[operator];
+        }
+
         return blsKeys[operator];
     }
 
@@ -29,6 +35,14 @@ abstract contract BLSKeyManager is MiddlewareStorage {
     function updateBLSKey(address operator, bytes memory key) external onlyOwner {
         if (blsKeyData[key].getAddress() != address(0)) {
             revert DuplicateBLSKey();
+        }
+
+        if (keccak256(blsKeys[operator]) != keccak256("")) {
+            if (blsKeyData[blsKeys[operator]].enabled == Time.timestamp()) {
+                revert FreshBLSKey();
+            }
+
+            prevBLSKeys[operator] = blsKeys[operator];
         }
 
         blsKeys[operator] = key;
--- src/KeyManager.sol
+++ src/KeyManager.sol
@@ -4,21 +4,27 @@ pragma solidity 0.8.25;
 import {Time} from "@openzeppelin/contracts/utils/types/Time.sol";
 
 import {MiddlewareStorage} from "./MiddlewareStorage.sol";
-import {ArrayWithTimes} from "./libraries/ArrayWithTimes.sol";
+import {EnumerableSetWithTimeData} from "./libraries/EnumerableSetWithTimeData.sol";
 
 abstract contract KeyManager is MiddlewareStorage {
-    using ArrayWithTimes for ArrayWithTimes.Address;
+    using EnumerableSetWithTimeData for EnumerableSetWithTimeData.Inner;
 
     error DuplicateKey();
+    error FreshKey();
 
     mapping(address => bytes32) public keys;
-    mapping(bytes32 => ArrayWithTimes.Address) internal keyData;
+    mapping(address => bytes32) public prevKeys;
+    mapping(bytes32 => EnumerableSetWithTimeData.Inner) internal keyData;
 
     function operatorByKey(bytes32 key) public view returns (address) {
         return keyData[key].getAddress();
     }
 
     function operatorKey(address operator) public view returns (bytes32) {
+        if (keyData[keys[operator]].enabled == Time.timestamp()) {
+            return prevKeys[operator];
+        }
+
         return keys[operator];
     }
 
@@ -29,6 +35,14 @@ abstract contract KeyManager is MiddlewareStorage {
     function updateKey(address operator, bytes32 key) external onlyOwner {
         if (keyData[key].getAddress() != address(0)) {
             revert DuplicateKey();
+        }
+
+        if (keys[operator] != bytes32(0)) {
+            if (keyData[keys[operator]].enabled == Time.timestamp()) {
+                revert FreshKey();
+            }
+
+            prevKeys[operator] = keys[operator];
         }
 
         keys[operator] = key;
