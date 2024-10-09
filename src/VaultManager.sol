7278b86a5dcce9e12a00f65269a9293764a883ad
georgiypetrov <petrovgeorgiy11@gmail.com>
2024-10-10 03:44:18 +0400

    refactor: changed timestamps to epoch

--- src/VaultManager.sol
+++ src/VaultManager.sol
@@ -13,13 +13,12 @@ import {IVetoSlasher} from "@symbiotic/interfaces/slasher/IVetoSlasher.sol";
 import {Time} from "@openzeppelin/contracts/utils/types/Time.sol";
 import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 
-import {MiddlewareStorage} from "./MiddlewareStorage.sol";
-import {EnumerableSetWithTimeData} from "./libraries/EnumerableSetWithTimeData.sol";
+import {BaseMiddleware} from "./BaseMiddleware.sol";
+import {PauseableEnumerableSet} from "./libraries/PauseableEnumerableSet.sol";
 
-abstract contract VaultManager is MiddlewareStorage {
-    using EnumerableSetWithTimeData for EnumerableSetWithTimeData.AddressSet;
-    using EnumerableSetWithTimeData for EnumerableSetWithTimeData.Uint160Set;
-
+abstract contract VaultManager is BaseMiddleware {
+    using PauseableEnumerableSet for PauseableEnumerableSet.AddressSet;
+    using PauseableEnumerableSet for PauseableEnumerableSet.Uint160Set;
     using Subnetwork for address;
 
     error NotVault();
@@ -27,19 +26,14 @@ abstract contract VaultManager is MiddlewareStorage {
     error VaultNotRegistered();
     error VaultAlreadyRegistred();
     error VaultEpochTooShort();
-
     error TooOldEpoch();
     error InvalidEpoch();
-
     error InvalidSubnetworksCnt();
-
     error UnknownSlasherType();
     error NonVetoSlasher();
 
-    EnumerableSetWithTimeData.Uint160Set subnetworks;
-
-    EnumerableSetWithTimeData.AddressSet internal sharedVaults;
-    mapping(address => EnumerableSetWithTimeData.AddressSet) internal operatorVaults;
+    PauseableEnumerableSet.AddressSet internal sharedVaults;
+    mapping(address => PauseableEnumerableSet.AddressSet) internal operatorVaults;
 
     struct SlashResponse {
         address vault;
@@ -48,46 +42,61 @@ abstract contract VaultManager is MiddlewareStorage {
         uint256 response; // if instant slashed amount else slash index
     }
 
-    constructor() {
-        subnetworks.register(DEFAULT_SUBNETWORK);
-    }
-
-    function subnetworksLength() external view returns (uint256) {
-        return subnetworks.length();
-    }
-
-    function subnetworkWithTimesAt(uint256 pos) external view returns (uint160, uint48, uint48) {
-        return subnetworks.at(pos);
-    }
-
+    /* 
+     * Returns the length of shared vaults.
+     * @return The number of shared vaults.
+     */
     function sharedVaultsLength() external view returns (uint256) {
         return sharedVaults.length();
     }
 
-    function sharedVaultWithTimesAt(uint256 pos) external view returns (address, uint48, uint48) {
+    /* 
+     * Returns the address and epoch information of a shared vault at a specific position.
+     * @param pos The index position in the shared vaults array.
+     * @return The address, enabled epoch, and disabled epoch of the vault.
+     */
+    function sharedVaultWithEpochsAt(uint256 pos) external view returns (address, uint48, uint48) {
         return sharedVaults.at(pos);
     }
 
+    /* 
+     * Returns the length of operator vaults for a specific operator.
+     * @param operator The address of the operator.
+     * @return The number of vaults associated with the operator.
+     */
     function operatorVaultsLength(address operator) external view returns (uint256) {
         return operatorVaults[operator].length();
     }
 
-    function opeartorVaultWithTimesAt(address operator, uint256 pos) external view returns (address, uint48, uint48) {
+    /* 
+     * Returns the address and epoch information of an operator vault at a specific position.
+     * @param operator The address of the operator.
+     * @param pos The index position in the operator vaults array.
+     * @return The address, enabled epoch, and disabled epoch of the vault.
+     */
+    function operatorVaultWithEpochsAt(address operator, uint256 pos) external view returns (address, uint48, uint48) {
         return operatorVaults[operator].at(pos);
     }
 
-    // useful when vaults have different assets
+    /* 
+     * Converts stake to power for a vault.
+     * @param vault The address of the vault.
+     * @param stake The amount of stake to convert.
+     * @return The power calculated from the stake.
+     */
     function stakeToPower(address vault, uint256 stake) public view virtual returns (uint256) {
         return stake;
     }
 
-    function activeSubnetworks(uint48 timestamp) public view returns (uint160[] memory) {
-        return subnetworks.getActive(timestamp);
-    }
-
-    function activeVaults(address operator, uint48 timestamp) public view returns (address[] memory) {
-        address[] memory activeSharedVaults = sharedVaults.getActive(timestamp);
-        address[] memory activeOperatorVaults = operatorVaults[operator].getActive(timestamp);
+    /* 
+     * Returns the list of active vaults for a specific operator.
+     * @param operator The address of the operator.
+     * @return An array of addresses representing the active vaults.
+     */
+    function activeVaults(address operator) public view returns (address[] memory) {
+        uint48 epoch = getCurrentEpoch();
+        address[] memory activeSharedVaults = sharedVaults.getActive(epoch);
+        address[] memory activeOperatorVaults = operatorVaults[operator].getActive(epoch);
 
         uint256 activeSharedVaultsLen = activeSharedVaults.length;
         address[] memory vaults = new address[](activeSharedVaultsLen + activeOperatorVaults.length);
@@ -101,43 +110,86 @@ abstract contract VaultManager is MiddlewareStorage {
         return vaults;
     }
 
+    /* 
+     * Registers a new shared vault.
+     * @param vault The address of the vault to register.
+     */
     function registerSharedVault(address vault) external onlyOwner {
         _validateVault(vault);
-        sharedVaults.register(vault);
+        sharedVaults.register(getNextEpoch(), vault);
     }
 
+    /* 
+     * Registers a new operator vault.
+     * @param vault The address of the vault to register.
+     * @param operator The address of the operator.
+     */
     function registerOperatorVault(address vault, address operator) external onlyOwner {
         _validateVault(vault);
-        operatorVaults[operator].register(vault);
+        operatorVaults[operator].register(getNextEpoch(), vault);
     }
 
+    /* 
+     * Pauses a shared vault.
+     * @param vault The address of the vault to pause.
+     */
     function pauseSharedVault(address vault) external onlyOwner {
-        sharedVaults.pause(vault);
+        sharedVaults.pause(getNextEpoch(), vault);
     }
 
+    /* 
+     * Unpauses a shared vault.
+     * @param vault The address of the vault to unpause.
+     */
     function unpauseSharedVault(address vault) external onlyOwner {
-        sharedVaults.unpause(vault, SLASHING_WINDOW);
+        sharedVaults.unpause(getNextEpoch(), SLASHING_WINDOW, vault);
     }
 
+    /* 
+     * Pauses an operator vault.
+     * @param operator The address of the operator.
+     * @param vault The address of the vault to pause.
+     */
     function pauseOperatorVault(address operator, address vault) external onlyOwner {
-        operatorVaults[operator].pause(vault);
+        operatorVaults[operator].pause(getNextEpoch(), vault);
     }
 
+    /* 
+     * Unpauses an operator vault.
+     * @param operator The address of the operator.
+     * @param vault The address of the vault to unpause.
+     */
     function unpauseOperatorVault(address operator, address vault) external onlyOwner {
-        operatorVaults[operator].unpause(vault, SLASHING_WINDOW);
+        operatorVaults[operator].unpause(getNextEpoch(), SLASHING_WINDOW, vault);
     }
 
+    /* 
+     * Unregisters a shared vault.
+     * @param vault The address of the vault to unregister.
+     */
     function unregisterSharedVault(address vault) external onlyOwner {
-        sharedVaults.unregister(vault, SLASHING_WINDOW);
+        sharedVaults.unregister(getNextEpoch(), SLASHING_WINDOW, vault);
     }
 
+    /* 
+     * Unregisters an operator vault.
+     * @param operator The address of the operator.
+     * @param vault The address of the vault to unregister.
+     */
     function unregisterOperatorVault(address operator, address vault) external onlyOwner {
-        operatorVaults[operator].unregister(vault, SLASHING_WINDOW);
+        operatorVaults[operator].unregister(getNextEpoch(), SLASHING_WINDOW, vault);
     }
 
-    function getOperatorStake(address operator, uint48 timestamp) public view returns (uint256 stake) {
-        address[] memory vaults = activeVaults(operator, timestamp);
-        uint160[] memory _subnetworks = activeSubnetworks(timestamp);
+    /* 
+     * Returns the stake of an operator at a specific epoch.
+     * @param epoch The epoch to check.
+     * @param operator The address of the operator.
+     * @return The stake of the operator.
+     */
+    function getOperatorStake(uint48 epoch, address operator) public view returns (uint256 stake) {
+        uint48 timestamp = getEpochStart(epoch);
+        address[] memory vaults = activeVaults(operator);
+        uint160[] memory _subnetworks = activeSubnetworks();
 
         for (uint256 i; i < vaults.length; ++i) {
             address vault = vaults[i];
@@ -150,9 +202,16 @@ abstract contract VaultManager is MiddlewareStorage {
         return stake;
     }
 
-    function getOperatorPower(address operator, uint48 timestamp) public view returns (uint256 power) {
-        address[] memory vaults = activeVaults(operator, timestamp);
-        uint160[] memory _subnetworks = activeSubnetworks(timestamp);
+    /* 
+     * Returns the power of an operator at a specific epoch.
+     * @param epoch The epoch to check.
+     * @param operator The address of the operator.
+     * @return The power of the operator.
+     */
+    function getOperatorPower(uint48 epoch, address operator) public view returns (uint256 power) {
+        uint48 timestamp = getEpochStart(epoch);
+        address[] memory vaults = activeVaults(operator);
+        uint160[] memory _subnetworks = activeSubnetworks();
 
         for (uint256 i; i < vaults.length; ++i) {
             address vault = vaults[i];
@@ -166,23 +225,31 @@ abstract contract VaultManager is MiddlewareStorage {
         return power;
     }
 
-    function totalStake(uint48 timestamp, address[] memory operators) internal view returns (uint256 stake) {
-        if (timestamp < Time.timestamp() - SLASHING_WINDOW) {
-            revert TooOldEpoch();
-        }
-
-        if (timestamp > Time.timestamp()) {
-            revert InvalidEpoch();
-        }
-
+    /* 
+     * Returns the total stake of multiple operators at a specific epoch.
+     * @param epoch The epoch to check.
+     * @param operators The list of operator addresses.
+     * @return The total stake of the operators.
+     */
+    function totalStake(uint48 epoch, address[] memory operators) internal view returns (uint256 stake) {
         for (uint256 i; i < operators.length; ++i) {
-            uint256 operatorStake = getOperatorStake(operators[i], timestamp);
+            uint256 operatorStake = getOperatorStake(epoch, operators[i]);
             stake += operatorStake;
         }
 
         return stake;
     }
 
+    /* 
+     * Slashes a vault based on provided conditions.
+     * @param timestamp The timestamp when the slash occurs.
+     * @param vault The address of the vault.
+     * @param subnetwork The subnetwork identifier.
+     * @param operator The operator to slash.
+     * @param amount The amount to slash.
+     * @param hints Additional data for the slasher.
+     * @return A struct containing information about the slash response.
+     */
     function slashVault(
         uint48 timestamp,
         address vault,
@@ -209,12 +276,20 @@ abstract contract VaultManager is MiddlewareStorage {
         }
     }
 
-    function executeSlash(address vault, uint256 slashIndex, bytes calldata hints)
+    /* 
+     * Executes a veto-based slash for a vault.
+     * @param vault The address of the vault.
+     * @param operator The address of the operator.
+     * @param slashIndex The index of the slash to execute.
+     * @param hints Additional data for the veto slasher.
+     * @return The amount that was slashed.
+     */
+    function executeSlash(address vault, address operator, uint256 slashIndex, bytes calldata hints)
         external
         onlyOwner
         returns (uint256 slashedAmount)
     {
-        if (!sharedVaults.contains(vault)) {
+        if (!(sharedVaults.contains(vault) || operatorVaults[operator].contains(vault))) {
             revert NotVault();
         }
 
@@ -227,6 +302,10 @@ abstract contract VaultManager is MiddlewareStorage {
         return IVetoSlasher(slasher).executeSlash(slashIndex, hints);
     }
 
+    /* 
+     * Validates if the vault is properly initialized and registered.
+     * @param vault The address of the vault to validate.
+     */
     function _validateVault(address vault) private view {
         if (!IRegistry(VAULT_REGISTRY).isEntity(vault)) {
             revert NotVault();
--- src/examples/simple-network/SimpleMiddleware.sol
+++ src/examples/simple-network/SimpleMiddleware.sol
@@ -12,24 +12,31 @@ import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 import {VaultManager} from "../../VaultManager.sol";
 import {OperatorManager} from "../../OperatorManager.sol";
 import {KeyManager} from "../../KeyManager.sol";
-import {MiddlewareStorage} from "../..//MiddlewareStorage.sol";
+import {BaseMiddleware} from "../..//BaseMiddleware.sol";
 
 contract SimpleMiddleware is VaultManager, OperatorManager, KeyManager {
     using Subnetwork for address;
 
-    error SlashingWindowTooShort();
-    error InactiveKeySlash();
-    error NotExistKeySlash();
-    error InvalidHints();
+    error InactiveKeySlash(); // Error thrown when trying to slash an inactive key
+    error NotExistKeySlash(); // Error thrown when the key does not exist for slashing
+    error InvalidHints(); // Error thrown for invalid hints provided
 
     struct ValidatorData {
-        uint256 power;
-        bytes32 key;
+        uint256 power; // Power of the validator
+        bytes32 key; // Key associated with the validator
     }
 
-    uint48 public immutable EPOCH_DURATION;
-    uint48 public immutable START_TIME;
-
+    /* 
+     * Constructor for initializing the SimpleMiddleware contract.
+     * @param network The address of the network.
+     * @param operatorRegistry The address of the operator registry.
+     * @param vaultRegistry The address of the vault registry.
+     * @param operatorNetOptin The address of the operator network opt-in service.
+     * @param owner The address of the contract owner.
+     * @param epochDuration The duration of each epoch.
+     * @param slashingWindow The duration of the slashing window. It's an epochDuration multuplier
+     * slashingWindowDuration = slashingWindow * epochDuration
+     */
     constructor(
         address network,
         address operatorRegistry,
@@ -38,65 +45,58 @@ contract SimpleMiddleware is VaultManager, OperatorManager, KeyManager {
         address owner,
         uint48 epochDuration,
         uint48 slashingWindow
-    ) MiddlewareStorage(owner, network, slashingWindow, vaultRegistry, operatorRegistry, operatorNetOptin) {
-        if (slashingWindow < epochDuration) {
-            revert SlashingWindowTooShort();
-        }
+    )
+        BaseMiddleware(owner, network, epochDuration, slashingWindow, vaultRegistry, operatorRegistry, operatorNetOptin)
+    {}
 
-        EPOCH_DURATION = epochDuration;
-        SLASHING_WINDOW = slashingWindow;
-    }
-
-    function getEpochStart(uint48 epoch) public view returns (uint48 timestamp) {
-        return START_TIME + epoch * EPOCH_DURATION;
-    }
-
-    function getEpochAt(uint48 timestamp) public view returns (uint48 epoch) {
-        return (timestamp - START_TIME) / EPOCH_DURATION;
-    }
-
-    function getCurrentEpoch() public view returns (uint48 epoch) {
-        return getEpochAt(Time.timestamp());
-    }
-
-    function getCurrentEpochStart() public view returns (uint48 timestamp) {
-        return START_TIME + getCurrentEpoch() * EPOCH_DURATION;
-    }
-
+    /* 
+     * Returns the total stake for the active operators in the current epoch.
+     * @return The total stake amount.
+     */
     function getTotalStake() public view returns (uint256) {
-        uint48 epochStart = getCurrentEpochStart();
-        address[] memory operators = activeOperators(epochStart);
-
-        return totalStake(epochStart, operators);
+        address[] memory operators = activeOperators(); // Get the list of active operators
+        return totalStake(getCurrentEpoch(), operators); // Return the total stake for the current epoch
     }
 
+    /* 
+     * Returns the current validator set as an array of ValidatorData.
+     * @return An array of ValidatorData containing the power and key of each validator.
+     */
     function getValidatorSet() public view returns (ValidatorData[] memory validatorSet) {
-        uint48 epochStart = getCurrentEpochStart();
-
-        address[] memory operators = activeOperators(epochStart);
-        validatorSet = new ValidatorData[](operators.length);
-        uint256 len = 0;
+        uint48 epoch = getCurrentEpoch(); // Get the current epoch
+        address[] memory operators = activeOperators(); // Get the list of active operators
+        validatorSet = new ValidatorData[](operators.length); // Initialize the validator set
+        uint256 len = 0; // Length counter
 
         for (uint256 i; i < operators.length; ++i) {
-            address operator = operators[i];
+            address operator = operators[i]; // Get the operator address
 
-            bytes32 key = operatorKey(operator);
-            if (key == bytes32(0) || !keyWasActiveAt(key, epochStart)) {
-                continue;
+            bytes32 key = operatorKey(operator); // Get the key for the operator
+            if (key == bytes32(0) || !keyWasActiveAt(epoch, key)) {
+                continue; // Skip if the key is inactive
             }
 
-            uint256 power = getOperatorPower(operator, epochStart);
-            validatorSet[len++] = ValidatorData(power, key);
+            uint256 power = getOperatorPower(epoch, operator); // Get the operator's power
+            validatorSet[len++] = ValidatorData(power, key); // Store the validator data
         }
 
         assembly ("memory-safe") {
-            mstore(validatorSet, len)
+            mstore(validatorSet, len) // Update the length of the array
         }
     }
 
-    // Here are the hints getter
-    // https://github.com/symbioticfi/core/blob/main/src/contracts/hints/SlasherHints.sol
-    // https://github.com/symbioticfi/core/blob/main/src/contracts/hints/DelegatorHints.sol
+    /* 
+     * Slashes a validator based on the provided parameters.
+     * Here are the hints getter
+     * https://github.com/symbioticfi/core/blob/main/src/contracts/hints/SlasherHints.sol
+     * https://github.com/symbioticfi/core/blob/main/src/contracts/hints/DelegatorHints.sol
+     * @param epoch The epoch for which the slashing occurs.
+     * @param key The key of the operator to slash.
+     * @param amount The amount to slash.
+     * @param stakeHints Hints for determining stakes.
+     * @param slashHints Hints for the slashing process.
+     * @return An array of SlashResponse indicating the results of the slashing.
+     */
     function slash(
         uint48 epoch,
         bytes32 key,
@@ -104,51 +104,54 @@ contract SimpleMiddleware is VaultManager, OperatorManager, KeyManager {
         bytes[][] calldata stakeHints,
         bytes[] calldata slashHints
     ) public onlyOwner returns (SlashResponse[] memory slashResponses) {
-        uint48 epochStart = getEpochStart(epoch);
-        address operator = operatorByKey(key);
+        uint48 epochStart = getEpochStart(epoch); // Get the start timestamp for the epoch
+        address operator = operatorByKey(key); // Get the operator associated with the key
 
         if (operator == address(0)) {
-            revert NotExistKeySlash();
+            revert NotExistKeySlash(); // Revert if the operator does not exist
         }
 
-        if (!keyWasActiveAt(key, epochStart)) {
-            revert InactiveKeySlash();
+        if (!keyWasActiveAt(epoch, key)) {
+            revert InactiveKeySlash(); // Revert if the key is inactive
         }
 
-        uint256 totalStake = getOperatorStake(operator, epochStart);
-        address[] memory vaults = activeVaults(operator, epochStart);
-        uint160[] memory _subnetworks = activeSubnetworks(epochStart);
+        uint256 totalStake = getOperatorStake(epoch, operator); // Get the total stake for the operator
+        address[] memory vaults = activeVaults(operator); // Get active vaults for the operator
+        uint160[] memory _subnetworks = activeSubnetworks(); // Get active subnetworks
 
-        slashResponses = new SlashResponse[](vaults.length * _subnetworks.length);
-        uint256 len = 0;
+        slashResponses = new SlashResponse[](vaults.length * _subnetworks.length); // Initialize the array for slash responses
+        uint256 len = 0; // Length counter
 
         if (stakeHints.length != slashHints.length || stakeHints.length != vaults.length) {
-            revert InvalidHints();
+            revert InvalidHints(); // Revert if the hints do not match in length
         }
 
         for (uint256 i; i < vaults.length; ++i) {
             if (stakeHints[i].length != _subnetworks.length) {
-                revert InvalidHints();
+                revert InvalidHints(); // Revert if the stake hints do not match the subnetworks
             }
 
-            address vault = vaults[i];
+            address vault = vaults[i]; // Get the vault address
             for (uint256 j = 0; j < _subnetworks.length; ++j) {
-                bytes32 subnetwork = NETWORK.subnetwork(uint96(_subnetworks[j]));
+                bytes32 subnetwork = NETWORK.subnetwork(uint96(_subnetworks[j])); // Get the subnetwork
                 uint256 stake = IBaseDelegator(IVault(vault).delegator()).stakeAt(
-                    subnetwork, operator, epochStart, stakeHints[i][j]
+                    subnetwork,
+                    operator,
+                    epochStart,
+                    stakeHints[i][j] // Get the stake at the specified subnetwork
                 );
 
-                uint256 slashAmount = Math.mulDiv(amount, stake, totalStake);
+                uint256 slashAmount = Math.mulDiv(amount, stake, totalStake); // Calculate the slashing amount
                 if (slashAmount == 0) {
-                    continue;
+                    continue; // Skip if the slashing amount is zero
                 }
 
-                slashResponses[len++] = slashVault(epochStart, vault, subnetwork, operator, slashAmount, slashHints[i]);
+                slashResponses[len++] = slashVault(epochStart, vault, subnetwork, operator, slashAmount, slashHints[i]); // Execute the slashing
             }
         }
 
         assembly ("memory-safe") {
-            mstore(slashResponses, len)
+            mstore(slashResponses, len) // Update the length of the slash responses
         }
     }
 }
