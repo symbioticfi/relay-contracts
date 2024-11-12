db3e8d1be1ed1993816e13b7ea10641b18f089e5
Algys Ievlev <algys.ievlev@gmail.com>
2024-11-12 11:46:56 +0700

    feat: add oz access managed

---
+++ src/middleware/extensions/OzAccessManaged.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import {AccessManagedUpgradeable} from "@openzeppelin/contracts-upgradeable/access/manager/AccessManagedUpgradeable.sol";
+
+import {BaseMiddleware} from "../BaseMiddleware.sol";
+
+abstract contract OzAccessManaged is BaseMiddleware, AccessManagedUpgradeable {
+    constructor(address authority) {
+        __AccessManaged_init(authority);
+    }
+
+    function _checkAccess() internal override {
+        _checkCanCall(msg.sender, msg.data);
+    }
+}
