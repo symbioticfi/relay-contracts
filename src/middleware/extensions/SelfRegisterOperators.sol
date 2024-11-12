2141ea27a4cdd175d03cec8120cc1d4eac4a4277
georgiypetrov <petrovgeorgiy11@gmail.com>
2024-11-12 11:52:50 +0700

    fix: selfregisteroperator

--- src/middleware/extensions/SelfRegisterOperators.sol
+++ src/middleware/extensions/SelfRegisterOperators.sol
@@ -4,24 +4,24 @@ pragma solidity ^0.8.25;
 import {BaseMiddleware} from "../BaseMiddleware.sol";
 
 abstract contract Operators is BaseMiddleware {
-    function registerOperator(address operator, bytes memory key, address vault) public checkAccess {
+    function registerOperator(address operator, bytes memory key, address vault) public {
         _beforeRegisterOperator(operator, key, vault);
         _registerOperator(operator);
         _updateKey(operator, key);
         _registerOperatorVault(operator, vault);
     }
 
-    function unregisterOperator(address operator) public checkAccess {
+    function unregisterOperator(address operator) public {
         _beforeUnregisterOperator(operator);
         _unregisterOperator(operator);
     }
 
-    function pauseOperator(address operator) public checkAccess {
+    function pauseOperator(address operator) public {
         _beforePauseOperator(operator);
         _pauseOperator(operator);
     }
 
-    function unpauseOperator(address operator) public checkAccess {
+    function unpauseOperator(address operator) public {
         _beforeUnpauseOperator(operator);
         _unpauseOperator(operator);
     }
