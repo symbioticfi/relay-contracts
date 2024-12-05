81c65f140f653d8b9dd1245c246a8c802c44ce5e
alrxy <georgy@symbiotic.fi>
2024-12-05 18:59:04 +0400

    chore: clean base middleware

--- src/middleware/BaseMiddleware.sol
+++ src/middleware/BaseMiddleware.sol
@@ -5,7 +5,6 @@ import {VaultManager} from "../managers/base/VaultManager.sol";
 import {OperatorManager} from "../managers/base/OperatorManager.sol";
 import {AccessManager} from "../managers/base/AccessManager.sol";
 import {KeyManager} from "../managers/base/KeyManager.sol";
-// import {ViewHelper} from "./ViewHelper.sol";
 
 /**
  * @title BaseMiddleware
@@ -19,18 +18,4 @@ import {KeyManager} from "../managers/base/KeyManager.sol";
  * This contract serves as a foundation for building custom middleware by providing essential
  * management capabilities that can be extended with additional functionality.
  */
-abstract contract BaseMiddleware is VaultManager, OperatorManager, AccessManager, KeyManager {
-// function __BaseMiddleware_init() internal initializer {
-//     // Deploy view helper contract for delegating view functions
-//     address viewHelper = address(new ViewHelper());
-//     // _setViewHelper(viewHelper);
-// }
-
-// fallback() external {
-//     (bool success, bytes memory returndata) = address(new ViewHelper()).delegatecall(msg.data);
-//     bytes memory revertData = abi.encode(success, returndata);
-//     assembly {
-//         revert(add(32, revertData), mload(revertData))
-//     }
-// }
-}
+abstract contract BaseMiddleware is VaultManager, OperatorManager, AccessManager, KeyManager {}
