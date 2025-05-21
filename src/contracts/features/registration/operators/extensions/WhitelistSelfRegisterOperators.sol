7d74c0ac22b4db6dbdc5ca30a00dddb2b003ba1e
Andrey <andrey@symbiotic.fi>
2025-05-21 05:56:31 +0400

    feat: add operators registration whitelist feature

---
+++ src/contracts/features/registration/operators/extensions/WhitelistSelfRegisterOperators.sol
@@ -0,0 +1,123 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import {SelfRegisterOperators} from "../SelfRegisterOperators.sol";
+
+import {IWhitelistSelfRegisterOperators} from
+    "../../../../../interfaces/features/registration/operators/extensions/IWhitelistSelfRegisterOperators.sol";
+
+abstract contract WhitelistSelfRegisterOperators is SelfRegisterOperators, IWhitelistSelfRegisterOperators {
+    // keccak256(abi.encode(uint256(keccak256("symbiotic.storage.WhitelistSelfRegisterOperators")) - 1)) & ~bytes32(uint256(0xff))
+    bytes32 private constant WhitelistSelfRegisterOperatorsStorageLocation =
+        0x25b93ff8b4a329091c8d52079f1be19dcd6e61195383d70f54f5235ddae87400;
+
+    function _getWhitelistStorage() internal pure returns (WhitelistSelfRegisterOperatorsStorage storage $) {
+        bytes32 location = WhitelistSelfRegisterOperatorsStorageLocation;
+        assembly {
+            $.slot := location
+        }
+    }
+
+    function __WhitelistSelfRegisterOperators_init(
+        WhitelistSelfRegisterOperatorsInitParams memory initParams
+    ) internal virtual onlyInitializing {
+        WhitelistSelfRegisterOperatorsStorage storage $ = _getWhitelistStorage();
+        $._isWhitelistEnabled = initParams.isWhitelistEnabled;
+    }
+
+    /**
+     * @inheritdoc IWhitelistSelfRegisterOperators
+     */
+    function isWhitelistEnabled() public view virtual returns (bool) {
+        return _getWhitelistStorage()._isWhitelistEnabled;
+    }
+
+    /**
+     * @inheritdoc IWhitelistSelfRegisterOperators
+     */
+    function isOperatorWhitelisted(
+        address operator
+    ) public view virtual returns (bool) {
+        return _getWhitelistStorage()._whitelisted[operator];
+    }
+
+    /**
+     * @inheritdoc IWhitelistSelfRegisterOperators
+     */
+    function isOperatorVaultWhitelisted(address operator, address vault) public view virtual returns (bool) {
+        return _getWhitelistStorage()._whitelistedVault[operator][vault];
+    }
+
+    /**
+     * @inheritdoc IWhitelistSelfRegisterOperators
+     */
+    function setWhitelistStatus(
+        bool status
+    ) public virtual checkPermission {
+        _getWhitelistStorage()._isWhitelistEnabled = status;
+    }
+
+    /**
+     * @inheritdoc IWhitelistSelfRegisterOperators
+     */
+    function whitelistOperator(
+        address operator
+    ) public virtual checkPermission {
+        if (isOperatorWhitelisted(operator)) {
+            revert WhitelistSelfRegisterOperators_OperatorAlreadyWhitelisted();
+        }
+        _getWhitelistStorage()._whitelisted[operator] = true;
+    }
+
+    /**
+     * @inheritdoc IWhitelistSelfRegisterOperators
+     */
+    function whitelistOperatorVault(address operator, address vault) public virtual checkPermission {
+        if (isOperatorVaultWhitelisted(operator, vault)) {
+            revert WhitelistSelfRegisterOperators_OperatorVaultAlreadyWhitelisted();
+        }
+        _getWhitelistStorage()._whitelistedVault[operator][vault] = true;
+    }
+
+    /**
+     * @inheritdoc IWhitelistSelfRegisterOperators
+     */
+    function unwhitelistOperator(
+        address operator
+    ) public virtual checkPermission {
+        if (!isOperatorWhitelisted(operator)) {
+            revert WhitelistSelfRegisterOperators_OperatorNotWhitelisted();
+        }
+        _getWhitelistStorage()._whitelisted[operator] = false;
+        if (isWhitelistEnabled() && isOperatorActive(operator)) {
+            _unregisterOperator(operator);
+        }
+    }
+
+    /**
+     * @inheritdoc IWhitelistSelfRegisterOperators
+     */
+    function unwhitelistOperatorVault(address operator, address vault) public virtual checkPermission {
+        if (!isOperatorVaultWhitelisted(operator, vault)) {
+            revert WhitelistSelfRegisterOperators_OperatorVaultNotWhitelisted();
+        }
+        _getWhitelistStorage()._whitelistedVault[operator][vault] = false;
+        if (isWhitelistEnabled() && isOperatorVaultActive(operator, vault)) {
+            _unregisterOperatorVault(operator, vault);
+        }
+    }
+
+    function _registerOperatorImpl(address operator, address vault) internal virtual override {
+        if (isWhitelistEnabled() && !isOperatorWhitelisted(operator)) {
+            revert WhitelistSelfRegisterOperators_OperatorNotWhitelisted();
+        }
+        super._registerOperatorImpl(operator, vault);
+    }
+
+    function _registerOperatorVaultImpl(address operator, address vault) internal virtual override {
+        if (isWhitelistEnabled() && !isOperatorVaultWhitelisted(operator, vault)) {
+            revert WhitelistSelfRegisterOperators_OperatorVaultNotWhitelisted();
+        }
+        super._registerOperatorVaultImpl(operator, vault);
+    }
+}
---
+++ src/interfaces/features/registration/operators/extensions/IWhitelistSelfRegisterOperators.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {ISelfRegisterOperators} from "../ISelfRegisterOperators.sol";
+
+interface IWhitelistSelfRegisterOperators {
+    error WhitelistSelfRegisterOperators_OperatorNotWhitelisted();
+    error WhitelistSelfRegisterOperators_OperatorAlreadyWhitelisted();
+    error WhitelistSelfRegisterOperators_OperatorVaultNotWhitelisted();
+    error WhitelistSelfRegisterOperators_OperatorVaultAlreadyWhitelisted();
+
+    /// @custom:storage-location erc7201:symbiotic.storage.WhitelistSelfRegisterOperators
+    struct WhitelistSelfRegisterOperatorsStorage {
+        mapping(address => bool) _whitelisted;
+        mapping(address => mapping(address => bool)) _whitelistedVault;
+        bool _isWhitelistEnabled;
+    }
+
+    struct WhitelistSelfRegisterOperatorsInitParams {
+        bool isWhitelistEnabled;
+    }
+
+    function isWhitelistEnabled() external view returns (bool);
+
+    function isOperatorWhitelisted(
+        address operator
+    ) external view returns (bool);
+
+    function isOperatorVaultWhitelisted(address operator, address vault) external view returns (bool);
+
+    function setWhitelistStatus(
+        bool status
+    ) external;
+
+    function whitelistOperator(
+        address operator
+    ) external;
+
+    function whitelistOperatorVault(address operator, address vault) external;
+
+    function unwhitelistOperator(
+        address operator
+    ) external;
+
+    function unwhitelistOperatorVault(address operator, address vault) external;
+}
