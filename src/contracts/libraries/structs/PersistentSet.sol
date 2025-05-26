1f203d158d967f3f32b6c4b9069b3aa50d393951
Andrey <andrey@symbiotic.fi>
2025-05-27 00:05:46 +0400

    style: minor

--- src/contracts/libraries/structs/PersistentSet.sol
+++ src/contracts/libraries/structs/PersistentSet.sol
@@ -1,7 +1,4 @@
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v5.3.0) (utils/structs/EnumerableSet.sol)
-// This file was procedurally generated from scripts/generate/templates/EnumerableSet.js.
-
 pragma solidity ^0.8.20;
 
 import {Checkpoints} from "../structs/Checkpoints.sol";
