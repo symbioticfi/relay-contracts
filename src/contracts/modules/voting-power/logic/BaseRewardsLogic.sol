05238720cbb2c5a61f6b90beaa00612b444da631
Andrey <andrey@symbiotic.fi>
2025-06-11 22:47:35 +0400

    fix: approve token before distributions

--- src/contracts/modules/voting-power/logic/BaseRewardsLogic.sol
+++ src/contracts/modules/voting-power/logic/BaseRewardsLogic.sol
@@ -8,13 +8,19 @@ import {IDefaultOperatorRewards} from
 import {IBaseRewards} from "../../../../interfaces/modules/voting-power/IBaseRewards.sol";
 import {INetworkManager} from "../../../../interfaces/base/INetworkManager.sol";
 
+import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
 library BaseRewardsLogic {
+    using SafeERC20 for IERC20;
+
     function distributeStakerRewards(address stakerRewards, address token, uint256 amount, bytes memory data) public {
+        IERC20(token).forceApprove(stakerRewards, amount);
         IStakerRewards(stakerRewards).distributeRewards(INetworkManager(address(this)).NETWORK(), token, amount, data);
         emit IBaseRewards.DistributeStakerRewards(stakerRewards, token, amount, data);
     }
 
     function distributeOperatorRewards(address operatorRewards, address token, uint256 amount, bytes32 root) public {
+        IERC20(token).forceApprove(operatorRewards, amount);
         IDefaultOperatorRewards(operatorRewards).distributeRewards(
             INetworkManager(address(this)).NETWORK(), token, amount, root
         );
