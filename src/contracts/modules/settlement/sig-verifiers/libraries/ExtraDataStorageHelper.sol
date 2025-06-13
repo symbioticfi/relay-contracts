e1cd12ba57d8e26c1df10f4bb850d21b50aac3a1
Andrey <andrey@symbiotic.fi>
2025-06-13 06:36:49 +0400

    refactor: optimize ExtraDataStorageHelper

--- src/contracts/modules/settlement/sig-verifiers/libraries/ExtraDataStorageHelper.sol
+++ src/contracts/modules/settlement/sig-verifiers/libraries/ExtraDataStorageHelper.sol
@@ -2,16 +2,16 @@
 pragma solidity ^0.8.25;
 
 library ExtraDataStorageHelper {
-    string public constant GLOBAL_KEY_PREFIX = "symbiotic.Settlement.extraData.";
+    bytes32 public constant GLOBAL_KEY_PREFIX_HASH = keccak256("symbiotic.Settlement.extraData.");
 
-    string public constant KEY_TAG_PREFIX = "keyTag.";
+    bytes32 public constant KEY_TAG_PREFIX_HASH = keccak256("keyTag.");
 
     function getKey(uint32 verificationType, bytes32 nameHash) internal pure returns (bytes32) {
-        return keccak256(abi.encode(GLOBAL_KEY_PREFIX, verificationType, nameHash));
+        return keccak256(abi.encode(GLOBAL_KEY_PREFIX_HASH, verificationType, nameHash));
     }
 
     function getKey(uint32 verificationType, uint8 keyTag, bytes32 nameHash) internal pure returns (bytes32) {
-        return keccak256(abi.encode(GLOBAL_KEY_PREFIX, verificationType, KEY_TAG_PREFIX, keyTag, nameHash));
+        return keccak256(abi.encode(GLOBAL_KEY_PREFIX_HASH, verificationType, KEY_TAG_PREFIX_HASH, keyTag, nameHash));
     }
 
     function getKey(
@@ -21,8 +21,9 @@ library ExtraDataStorageHelper {
         uint256 index
     ) internal pure returns (bytes32) {
         return bytes32(
-            uint256(keccak256(abi.encode(GLOBAL_KEY_PREFIX, verificationType, KEY_TAG_PREFIX, keyTag, nameHash)))
-                + index
+            uint256(
+                keccak256(abi.encode(GLOBAL_KEY_PREFIX_HASH, verificationType, KEY_TAG_PREFIX_HASH, keyTag, nameHash))
+            ) + index
         );
     }
 }
